7d52a569b6b7a8374c1127107892ccb94219ada6
Lukáš Bastián <32299292+bastianluk@users.noreply.github.com>
2019-06-06 19:35:34 +0200

    Updated OPTIONS and skeleton

--- labmonitor.sh
+++ labmonitor.sh
@@ -5,16 +5,29 @@ set -x
 usage(){
 cat <<EOF
 SYNTAXE
-        ./labmonitor.sh [OPTIONS] TARGETS
+        ./labmonitor.sh [OPTIONS] FILE
 POPIS
-        The scripts spreads to assigned TARGETS and mointors users logged on to each of the TARGETS and after 
-        the end of its run it returns a table of logins with the time they logged in at and the duration they 
-        were logged in for
+        The scripts spreads to assigned adresses in FILE and mointors users logged on to each of the assigned 
+        adresses in FILE and after the end of its run it returns a table of logins with the time they logged 
+        in at and the duration they were logged in for
 
         #TODO
         OPTIONS:
         -s
-                Short format print for the table of results
+                Sort
+                
+                Example:
+                
+                
+                FLAGS:
+                a     Adress of PC the login belonged to
+                n     Login/Name
+                t     Time of the login it belonged to
+                o     Time spent online/logged in
+        -q
+                Quiet/Silent
+        -v
+                Verbose
         -f
                 Format change for the output table
 
@@ -23,15 +36,30 @@ POPIS
 
                 FLAGS:
                 a     Adress of PC the login belonged to
+                n     Login/Name
                 t     Time of the login it belonged to
+                o     Time spent online/logged in
 EOF
 }
 
+
 #ARGs
-s=0
+#Quiet/Silent
+q=0
+v=0
+#Format info
+f=0
+fadr=1
+fname=1
 ftime=1
 fon=1
-fadr=1
+#Sort info
+s=0
+sadr=1
+sname=1
+stime=1
+son=1
+
 
 #FUNC
 getFormatFlags() {
@@ -40,19 +68,53 @@ getFormatFlags() {
   then
     fadr=1
   fi
+  if [[ "$1" =~ n ]]
+  then
+    fname=1
+  fi
+  if [[ "$1" =~ t ]]
+  then
+    ftime=1
+  fi
   if [[ "$1" =~ o ]]
   then
     fon=1
   fi
-
-  #TODO
 }
-
 formatErase() {
+  fadr=0
+  fname=0
   ftime=0
   fon=0
-  fadr=0
 }
+
+#Sort info
+getSortFlags() {
+  sortErase
+  if [[ "$1" =~ a ]]
+  then
+    sadr=1
+  fi
+  if [[ "$1" =~ n ]]
+  then
+    sname=1
+  fi
+  if [[ "$1" =~ t ]]
+  then
+    stime=1
+  fi
+  if [[ "$1" =~ o ]]
+  then
+    son=1
+  fi
+}
+sortErase() {
+  sadr=0
+  sname=0
+  stime=0
+  son=0
+}
+
 
 #MAIN
 #Print help if needed
@@ -66,12 +128,32 @@ fi
 for options in $@
 do
   case "$option" in
+    -q)
+      #Quiet/Silent
+      if [ "$v" -eq 1 ]
+      then
+        echo "Overwriting verbose - now quiet."
+        v=0
+      fi
+      q=1
+      ;;
+    -v)
+      #Verbose
+      if [ "$q" -eq 1 ]
+      then
+        echo "Overwriting quiet/silent - now verbose."
+        q=0
+      fi
+      v=1
+      ;;
     -s)
-      #short table print
+      #Sort
       s=1
+      getSortFlags "$options"
       ;;
     -f)
       #Format change
+      f=1
       getFormatFlags "$options"
       ;;
     *)
@@ -82,3 +164,9 @@ do
 done
 
 #TODO
+#Spread
+
+#Await results
+
+#Compile results
+
