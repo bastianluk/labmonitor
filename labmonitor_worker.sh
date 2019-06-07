3dee756d2c8367c132151f2b14c8d321d65fc34b
Lukas Bastian <bastianl@u-pl13.ms.mff.cuni.cz>
2019-06-07 12:27:04 +0200

    Finished worker

--- labmonitor_worker.sh
+++ labmonitor_worker.sh
@@ -9,11 +9,11 @@
 usage() {
   cat <<EOF
 SYNTAX
-        ./labmonitor_worker.sh RETURN_ADDRESS
+        ./labmonitor_worker.sh RETURN_ADDRESS [END_TIME]
 DESCRIPTION
-        Monitors logins for the duration of a run and send results back to 
+        Monitors logins for the duration of a run and send results back to
         RETURN_ADDRESS
-  EOF
+EOF
 }
 
 #DEBUG
@@ -21,26 +21,64 @@ DESCRIPTION
 #set -x
 
 #ARGs
+dateStart=$(date +%Y-%m-%d\ %H:%M)
+dateEndDay=''
+dateEnd=''
 fileName=$(echo "res_"$(hostname -f))
+address="$1"
 
 #FUNC
-#Login check
-checkNew() {
-  
+killHandle(){
+  getResults
+
+  finish
+}
+
+timeHandle(){
+  echo "Here"
+  diff=$(echo "$(date -d "$dateEnd" "+%s")-$(date -d "$dateStart" "+%s")" | bc)
+  echo "$diff"
+  sleep "$diff"
+
+  getResults
+
+  finish
+}
+
+getResults(){
+  last -s "$dateStart" | tr -s ' ' | cut -d ' ' -f1,5- > tmp_labwork1
+  lines=$(cat tmp_labwork1 | wc -l)
+  for n in $(seq 1 1 "$lines")
+  do
+    echo $(hostname -f) >> tmp_labwork2
+  done
+  paste -d' ' tmp_labwork2 tmp_labwork1 > "$fileName"
 }
 
 #Finish
 finish() {
   #Send results
-  scp "$filename" "$1"
+  echo "$1"
+  scp "$fileName" "$address"
 
   #Cleanup
-  rm "$filename"
+  rm tmp_labwork*
+  rm "$fileName"
 }
 
-#MAIN
-echo "$1" > "$filename"
-hostname >> "$filename"
 
-#Before kill or end time - call finish
-finish
+#MAIN
+trap killHandle 9
+if [ "$#" -eq 1 ]
+then
+  while :
+  do
+    :
+    #Waiting for the program to finish via trap ~ PC turn off or ps kill
+  done
+elif [ "$#" -eq 2 ]
+then
+  dateEndDay=$(date +%Y\-%m\-%d)
+  dateEnd=$(echo "$dateEndDay" "$2")
+  timeHandle
+fi
